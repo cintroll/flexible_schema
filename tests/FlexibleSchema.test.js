505891682561081e3ade1f4bc0ee33f85085f7f8
Matheus Cintra <matheus.goncalves@alumni.usp.br>
2023-11-09 22:00:13 -0300

    feat adicionado teste para parametro invalido com mapping

--- tests/FlexibleSchema.test.js
+++ tests/FlexibleSchema.test.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const FlexibleSchema_1 = __importDefault(require("../src/FlexibleSchema"));
 const Exceptions_1 = require("../src/Exceptions");
 describe('Testing Flexible Schema Module', () => {
-    test('Test Convert MongoDB Schema without mapping', () => {
+    test('Test Convert database interface without mapping', () => {
         const reg_attributes = ['name', 'currentPrice', 'offerPrice', 'quantity'];
         const data = {
             name: 'cocacola 2l un',
@@ -33,7 +33,7 @@ describe('Testing Flexible Schema Module', () => {
         const flexible_schema = new FlexibleSchema_1.default(reg_attributes);
         expect(flexible_schema.convert(data)).toStrictEqual(result);
     });
-    test('Test Convert MongoDB Schema with mapping', () => {
+    test('Test Convert database interface with mapping', () => {
         const reg_attributes = [
             { name: 'name', map: 'name' },
             { name: 'currentPrice', map: 'currentPrice' },
@@ -65,8 +65,40 @@ describe('Testing Flexible Schema Module', () => {
         const flexible_schema = new FlexibleSchema_1.default(reg_attributes);
         expect(flexible_schema.convert(data)).toStrictEqual(result);
     });
-    test('Test Convert MongoDB Schema inexistent parameter', () => {
+    test('Test Convert database interface inexistent parameter', () => {
         const reg_attributes = ['name', 'currentPrice', 'offerPrice', 'quantity', 'invalidParam'];
+        const data = {
+            name: 'cocacola 2l un',
+            prices: {
+                currentPrice: 10,
+                offerPrice: 8
+            },
+            quantity: 29,
+            customAttributes: [
+                { name: 'ecommerceCurrentPrice', value: 12 },
+                { name: 'ecommerceOfferPrice', value: 0 },
+                { name: 'ecommerceName', value: 'Coca Cola Normal 2L' }
+            ]
+        };
+        const result = {
+            name: 'cocacola 2l un',
+            currentPrice: 10,
+            offerPrice: 8,
+            quantity: 29,
+            ecommerceCurrentPrice: 12,
+            ecommerceOfferPrice: 0,
+            ecommerceName: 'Coca Cola Normal 2L'
+        };
+        const flexible_schema = new FlexibleSchema_1.default(reg_attributes);
+        expect(() => flexible_schema.convert(data)).toThrow(Exceptions_1.UnknownParameter);
+    });
+    test('Test Convert database interface inexistent parameter with mapping', () => {
+        const reg_attributes = [
+            { name: 'name', map: 'name' },
+            { name: 'currentPrice', map: 'currentPrice' },
+            { name: 'offerPrice', map: 'currentPrice' },
+            { name: 'quantity', map: 'invalidParam' }
+        ];
         const data = {
             name: 'cocacola 2l un',
             prices: {
--- tests/FlexibleSchema.test.ts
+++ tests/FlexibleSchema.test.ts
@@ -3,7 +3,7 @@ import { UnknownParameter } from "../src/Exceptions"
 
 describe('Testing Flexible Schema Module', () => {
 
-    test('Test Convert MongoDB Schema without mapping', () => {
+    test('Test Convert database interface without mapping', () => {
         const reg_attributes = ['name', 'currentPrice', 'offerPrice', 'quantity'];
         const data = {
             name: 'cocacola 2l un',
@@ -32,7 +32,7 @@ describe('Testing Flexible Schema Module', () => {
         expect(flexible_schema.convert(data)).toStrictEqual(result);
     });
 
-    test('Test Convert MongoDB Schema with mapping', () => {
+    test('Test Convert database interface with mapping', () => {
         const reg_attributes = [
             { name: 'name', map: 'name' },
             { name: 'currentPrice', map: 'currentPrice' },
@@ -66,8 +66,42 @@ describe('Testing Flexible Schema Module', () => {
         expect(flexible_schema.convert(data)).toStrictEqual(result);
     });
 
-    test('Test Convert MongoDB Schema inexistent parameter', () => {
+    test('Test Convert database interface inexistent parameter', () => {
         const reg_attributes = ['name', 'currentPrice', 'offerPrice', 'quantity', 'invalidParam'];
+        const data = {
+            name: 'cocacola 2l un',
+            prices: { 
+                currentPrice: 10, 
+                offerPrice: 8 
+            }, 
+            quantity: 29,
+            customAttributes: [
+                { name: 'ecommerceCurrentPrice', value: 12 }, 
+                { name: 'ecommerceOfferPrice', value: 0 }, 
+                { name: 'ecommerceName', value: 'Coca Cola Normal 2L' }
+            ]
+        };
+        const result = { 
+            name: 'cocacola 2l un', 
+            currentPrice: 10, 
+            offerPrice: 8, 
+            quantity: 29,
+            ecommerceCurrentPrice: 12, 
+            ecommerceOfferPrice: 0, 
+            ecommerceName: 'Coca Cola Normal 2L' 
+        };        
+        
+        const flexible_schema = new FlexibleSchema(reg_attributes);
+        expect(() => flexible_schema.convert(data)).toThrow(UnknownParameter);
+    });
+
+    test('Test Convert database interface inexistent parameter with mapping', () => {
+        const reg_attributes = [
+            { name: 'name', map: 'name' },
+            { name: 'currentPrice', map: 'currentPrice' },
+            { name: 'offerPrice', map: 'currentPrice' },
+            { name: 'quantity', map: 'invalidParam' }
+        ];
         const data = {
             name: 'cocacola 2l un',
             prices: { 
